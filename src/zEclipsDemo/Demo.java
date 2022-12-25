d60fca9c1fc270e2250aec79a1101af30de40337
vishald21 <111678124+vishald21@users.noreply.github.com>
2022-12-25 10:21:02 +0530

    Update Demo.java

--- src/zEclipsDemo/Demo.java
+++ src/zEclipsDemo/Demo.java
@@ -8,14 +8,12 @@ public class Demo {
 		
 		System.out.println("vishal Doke");
 
-		System.out.println("vishal Doke");
+		
 
 	}
 private void sysou() {
 	// TODO Auto-generated method stub
 System.out.println("pull request");
 
-System.out.println("2ndtime");
-System.out.println("3rdtime");
 }
 }
