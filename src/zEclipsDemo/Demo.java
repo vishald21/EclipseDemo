c37fc54bde16eadba3cbabaf3d747534ce13dbbe
vishald21 <vishaldoke2133@gmail.com>
2022-12-27 10:32:03 +0530

    latest

--- src/zEclipsDemo/Demo.java
+++ src/zEclipsDemo/Demo.java
@@ -1,12 +1,38 @@
 package zEclipsDemo;
 
 public class Demo {
+	
+	
+      void master(){
+    	   
+    	   
+    	   System.out.println("this is master branch");
+       }
+   
+      
+      void deploy() {
+    	  
+    	  System.out.println("this is deploy branch");
+    	  
+      }
+  
+      
+      void feachar() {
+    	  
+    	  System.out.println("this is feacher branch");
+      }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		
-		System.out.println("vishal Doke");
+           Demo d=new Demo();
+           d.master();
+           d.deploy();
+           d.feachar();
+    
 
-		
-	}}
+    	
+     
+}
+
+}
